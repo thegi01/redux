8c431261ba66d267a2c40f9ce958b1024e860f5d
thegi <deokim77@gmail.com>
2019-02-10 01:50:24 +0900

    오타 수정

--- todoList/src/reducers/index.js
+++ todoList/src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
 import todos from './todos'
-import visibiltyFilter from './visibilityFilter'
+import visibilityFilter from './visibilityFilter'
 
 export default combineReducers({
     todos,
-    visibiltyFilter
+    visibilityFilter
 })
